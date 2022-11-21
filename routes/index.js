93c3dbd874fef10a70eb88abfaef50da58cc3827
StephenSingler <stephen.singler@gmail.com>
2022-11-20 18:07:49 -0800

    switching to single page, finished up add, delete, and edit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,29 +11,80 @@ router.get('/', function (req, res, next) {
         console.log("Getting error " + err);
         exit(1);
       }
-      //Query if the table exists
+      //Query if the table exists if not lets create it on the fly!
       db.all(`SELECT name FROM sqlite_master WHERE type='table' AND name='blog'`,
         (err, rows) => {
           if (rows.length === 1) {
+            console.log("Table exists!");
             db.all(`SELECT blog_id, blog_txt FROM blog`, (err, rows) => {
-              res.render('index.pug', { title: 'Express', data: rows });
+              console.log("Returning " + rows.length + " records");
+              res.render('index', { title: 'Express', data: rows });
             });
           } else {
+            console.log("Creating table and inserting some sample data");
             db.exec(`CREATE TABLE blog (
                      blog_id INTEGER PRIMARY KEY AUTOINCREMENT,
                      blog_txt text NOT NULL);
-
                       INSERT INTO blog (blog_txt)
                       VALUES ('entry 1'),
                              ('entry 2');`,
               () => {
                 db.all(`SELECT blog_id, blog_txt FROM blog`, (err, rows) => {
-                  res.render('index.pug', { title: 'Express', data: rows });
+                  res.render('index', { title: 'Express', data: rows });
                 });
               });
           }
         });
     });
+});
+
+router.post('/add', (req, res, next) => {
+  console.log("Adding blog entry to table");
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error: " + err);
+        exit(1);
+      }
+      console.log("Inserting: " + req.body.blog);
+      db.run("INSERT INTO blog (blog_txt) VALUES (?)", [req.body.blog]);
+      //redirect to homepage
+      res.redirect('/');
+    }
+  );
+});
+
+router.post('/delete', (req, res, next) => {
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error " + err);
+        exit(1);
+      }
+      console.log("deleting " + req.body.blog);
+      db.run("DELETE FROM blog WHERE blog_id= ?", [req.body.blog]);     
+      res.redirect('/');
+    }
+  );
+})
+
+router.post('/edit', (req, res, next) => {
+  console.log("Editing specified blog entry");
+  var db = new sqlite3.Database('mydb.sqlite3',
+    sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+    (err) => {
+      if (err) {
+        console.log("Getting error: " + err);
+        exit(1);
+      }
+      console.log("Editing: " + req.body.blog);
+      db.run("UPDATE blog SET blog_txt = ? WHERE blog_id = ?", [req.body.txt, req.body.blog_id]);
+      //redirect to homepage
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
